5a50c2ee6607bf34d58d6fc2e48684a90b5fd42d
Juss <286642846+jussray@users.noreply.github.com>
2026-08-23 17:56:05 -0400

    feat(verification-core): add contract errors

---
+++ packages/ultrathink-verification-core/src/errors.ts
@@ -0,0 +1,9 @@
+export class VerificationCoreContractError extends Error {
+  readonly code: string;
+
+  constructor(code: string, message: string) {
+    super(message);
+    this.name = 'VerificationCoreContractError';
+    this.code = code;
+  }
+}
